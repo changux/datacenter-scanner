e19ab2ee32e41c1685f2fa1cb4a38cf4d003c09c
Andrew Suderman <andrew.suderman@readytalk.com>
2017-12-09 12:24:42 -0700

    Moving print statements to logging module

--- scan.py
+++ scan.py
@@ -10,6 +10,8 @@ import sys
 import traceback
 import json
 import socket
+import time
+import logging
 
 # Paramiko client configuration
 UseGSSAPI = paramiko.GSS_AUTH_AVAILABLE             # enable "gssapi-with-mic" authentication, if supported by your python installation
@@ -19,35 +21,38 @@ DoGSSAPIKeyExchange = paramiko.GSS_AUTH_AVAILABLE   # enable "gssapi-kex" key ex
 PORT=22
 
 def main():
+    """
+    Utilizes all the below functions to scan a list of IPs
+    """
 
     # Get username and password
     username, password = get_user()
 
     # List of CIDRs to scan
-    scan_list = ['192.168.6.1-253']
+    scan_list = ['192.168.6.0/24']
 
-    # We want physical boxen
+    # We want physical boxen, we will keep them here
     physical_machines = []
 
     # Send the commands
     hosts = get_hosts(scan_list)
-    hosts = sorted(hosts.items(), key=lambda item: socket.inet_aton(item[0]))
-    print(hosts)
-    print("Found {0} Hosts".format(len(hosts)))
-    
+    hosts = sorted(hosts, key=lambda item: socket.inet_aton(item))
+    LOG.info("Found {0} Hosts".format(len(hosts)))
+
     for host in hosts:
         info = get_data(host, username, password)
         if info is not None:
             physical_machines = collect_physical_machines(info, physical_machines)
 
     print(json.dumps(physical_machines, indent=4, sort_keys=True))
+    LOG.info("Found {0} physical machines.".format(len(physical_machines)))
 
 
 def build_netbox_device (device_role, manufacturer, model_name, status, site):
     """
     Builds a JSON object that we can submit to netbox
     """
-    
+
     device = { 'device_role': device_role, 'manufacturer': manufacturer, 'model_name': model_name, 'status': status, 'site': site }
     return json.loads(device)
 
@@ -71,20 +76,19 @@ def get_hosts(cidrs):
     try:
         nm = nmap.PortScanner()         # instantiate nmap.PortScanner object
     except nmap.PortScannerError:
-        print('Nmap not found', sys.exc_info()[0])
+        LOG.error('Nmap not found', sys.exc_info()[0])
         sys.exit(1)
     except:
-        print("Unexpected error:", sys.exc_info()[0])
+        LOG.error("Unexpected error:", sys.exc_info()[0])
         sys.exit(1)
 
     hosts_list = []
     for cidr in cidrs:
-        print("Scanning {0}....".format(cidr))
-        print(nm.scan(hosts=cidr, arguments='-n -p 22 --open -sV'))
+        LOG.info("Scanning {0}....".format(cidr))
+        LOG.debug(nm.scan(hosts=cidr, arguments='-n -p 22 --open -sV -A'))
         cidr_hosts = [(x, nm[x]['tcp'][22]['state']) for x in nm.all_hosts()]
-        print(cidr_hosts)
         for host, status in cidr_hosts:
-            #print("Host: {0} is {1}".format(host,status))
+            LOG.deug("Host: {0} is {1}".format(host,status))
             if status == "open":
                 hosts_list.append(host)
     return hosts_list
@@ -105,38 +109,16 @@ def get_user():
     return(user, password)
 
 
-def agent_auth(transport, username):
-    """
-    Attempt to authenticate to the given transport using any of the private
-    keys available from an SSH agent.
-    """
-
-    agent = paramiko.Agent()
-    agent_keys = agent.get_keys()
-    if len(agent_keys) == 0:
-        return
-
-    for key in agent_keys:
-        print('Trying ssh-agent key %s' % hexlify(key.get_fingerprint()))
-        try:
-            transport.auth_publickey(username, key)
-            print('... success!')
-            return
-        except paramiko.SSHException:
-            print('... nope.')
-
-
 def get_data(endpoint, username, password):
     """
     Gets basic data from the specified endpoint.  Needs an ssh username and pass.
     Returns a JSON dict of the form:
     {
-        "asset_tag": "",
         "ipaddress": "",
         "is_virtual": "",
         "model": "",
         "name": "",
-        "serial": ""
+        "service_tag": ""
     }
     """
 
@@ -156,43 +138,55 @@ echo "}"
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        print('Connecting to {0}'.format(endpoint))
+        LOG.info('Connecting to {0}'.format(endpoint))
         if not UseGSSAPI and not DoGSSAPIKeyExchange:
-            client.connect(endpoint, PORT, username, password, timeout=3)
+            LOG.info("Basic key auth")
+            client.connect(endpoint, PORT, username, password, timeout=2)
         else:
             try:
+                LOG.info("gssapikeyexchane")
                 client.connect(endpoint, PORT, username, gss_auth=UseGSSAPI,
-                               gss_kex=DoGSSAPIKeyExchange, timeout=3)
+                               gss_kex=DoGSSAPIKeyExchange, timeout=2)
             except Exception:
-                # traceback.print_exc()
-                password = getpass.getpass('Password for %s@%s: ' % (username, endpoint))
-                client.connect(endpoint, PORT, username, password, timeout=3)
+                LOG.info("password auth")
+                client.connect(endpoint, PORT, username, password, timeout=2)
 
         stdin, stdout, stderr = client.exec_command(command)
+
+        timeout = 5 
+        endtime = time.time() + timeout
+        while not stdout.channel.eof_received:
+            time.sleep(1)
+            if time.time() > endtime:
+                stdout.channel.close()
+                break
         raw = stdout.read()
         if raw:
             decoded = raw.decode("utf-8")
             json_output = json.loads(decoded)
             client.close()
-            print("Successfully gathered data")
+            LOG.info("Success!")
             return json_output
         else:
             return None
 
     except paramiko.ssh_exception.AuthenticationException as e:
-        print('Could not auth with {0}'.format(endpoint))
+        LOG.info('Bad Auth')
         pass
     except paramiko.ssh_exception.BadHostKeyException as e:
-        print('Bad host key exception on {0}'.format(endpoint))
+        LOG.info('Bad Host Key')
         pass
     except paramiko.ssh_exception.NoValidConnectionsError as e:
-        print(e)
+        LOG.error(e)
         pass
     except paramiko.ssh_exception.SSHException as e:
-        print(e)
+        LOG.error(e)
+        pass
+    except ConnectionResetError as e:
+        LOG.error("Connection Reset Error")
         pass
     except Exception as e:
-        print('*** Caught exception: %s: %s' % (e.__class__, e))
+        LOG.error('*** Caught exception: %s: %s' % (e.__class__, e))
         traceback.print_exc()
         try:
             client.close()
@@ -201,4 +195,8 @@ echo "}"
         sys.exit(1)
 
 if __name__ == "__main__":
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+    LOG = logging.getLogger("datacenter_scanner")
+    LOG.setLevel(logging.INFO)
+    main()
     main()
